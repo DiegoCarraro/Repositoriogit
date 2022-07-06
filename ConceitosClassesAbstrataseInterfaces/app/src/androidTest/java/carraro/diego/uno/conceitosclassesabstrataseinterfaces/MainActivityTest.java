060341e9b4d4c934b0755328505322dec67894eb
DiegoCarraro <diegocarraro10@gmail.com>
2022-07-06 11:23:03 -0300

    Test via AndroidTest
    Teste usando o Android teste

---
+++ ConceitosClassesAbstrataseInterfaces/app/src/androidTest/java/carraro/diego/uno/conceitosclassesabstrataseinterfaces/MainActivityTest.java
@@ -0,0 +1,29 @@
+package carraro.diego.uno.conceitosclassesabstrataseinterfaces;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.regex.Matcher;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class MainActivityTest {
+
+    @Test
+    public void DeveriaMainActivityestarVisivel(){
+        ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.layoutDaMainActivity)).check(matches(isDisplayed()));
+
+
+    }
+
+}
